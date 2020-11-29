111096e8075d15bb986bf2d67fc86f00a7ebdfec
daniel <df.zee@hotmail.com>
2020-11-29 22:28:00 +0100

    spellbook zonder login af

--- app.py
+++ app.py
@@ -10,4 +10,4 @@ app.register_blueprint(routes)
 app.app_context().push()
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0')
--- routes/root.py
+++ routes/root.py
@@ -36,9 +36,9 @@ class SpellbookForm(FlaskForm):
                 return [ord(y) for y in x[0]]
 
         super().__init__()
-        db_match = [(f[1], c[1]) for f in vars(self).items()
-                    for c in vars(Spell).items() if f[0] == c[0] and '_' not in f[0]]
-        for field, column in db_match:
+        self.db_match = [(f[1], c[1]) for f in vars(self).items()
+                         for c in vars(Spell).items() if f[0] == c[0] and '_' not in f[0]]
+        for field, column in self.db_match:
             field.choices = {(x[0], x[0]) for x in Spell.query.with_entities(
                 column).distinct().all()}
             field.choices = sorted(field.choices, key=custom_sort)
@@ -72,6 +72,17 @@ class SpellTable(Table):
 @routes.route('/spellbook', methods=['GET', 'POST'])
 def spellbook():
     form = SpellbookForm()
-    spells = Spell.query.all()
-    table = SpellTable(spells)
+    total_query = Spell.query
+    if form.validate_on_submit():
+        for field, column in form.db_match:
+            if field.data:
+                build_query = Spell.query.filter(False)
+                if isinstance(field.data, list):
+                    for selection in field.data:
+                        build_query = build_query.union(total_query.filter(column == selection))
+                if isinstance(field.data, str):
+                    build_query = total_query.filter(column.contains(field.data))
+                total_query = build_query
+
+    table = SpellTable(total_query.all(), border=True)
     return render_template('spellbook.html', title='Spellbook', form=form, table=table)
