6c69f70b5595b8dd87ba1c0c368b36a5c8e8d53c
daniel <df.zee@hotmail.com>
2020-12-26 01:07:31 +0100

    met rebuild

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from config import Config
 from database import db, login
+from database.spellbook import rebuild
 from routes import routes, bootstrap
+import argparse
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -12,5 +14,10 @@ app.register_blueprint(routes)
 app.app_context().push()
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--rebuild', help='Rebuild spell database', action="store_true")
+    args = parser.parse_args()
+    if args.rebuild:
+        rebuild()
     db.create_all(bind="user_data")
     app.run(host='0.0.0.0', debug=True)
